cfc965508e34b59cdde999a5f2df8a57bbbf2b4c
Venu <venu.nh@gmail.com>
2017-10-06 21:49:36 +0530

    push to github

---
+++ src/main/java/com/test/jenkins/JenkinsProject/App.java
@@ -0,0 +1,13 @@
+package com.test.jenkins.JenkinsProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/test/jenkins/JenkinsProject/AppTest.java
@@ -0,0 +1,15 @@
+package com.test.jenkins.JenkinsProject;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+	public void testJenkins()
+	{
+		System.out.println("Jenkins is running successfully..");
+	}
+}
